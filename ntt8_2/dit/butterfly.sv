f3f99f0cda8e0f07d5a8494705a3a8a87638ced2
arogya <arogyadahal2003@gmail.com>
2026-07-17 08:45:59 -0400

    Montgomery Reduction in DIT

--- ntt8_2/dit/butterfly.sv
+++ ntt8_2/dit/butterfly.sv
@@ -14,9 +14,42 @@ parameter logic [2:0] WIDTH = 3'd5
     logic [WIDTH - 1:0]   mod_mul;  
     logic [WIDTH:0]     sum;  
 
+    logic [WIDTH:0] reducer = 1 << WIDTH; // this is the R
+    logic [WIDTH:0] r_inv = 6'd8; // R^-1, calculated this using FLT a^{p - 2} (mod 17)
+    logic [WIDTH:0] k = ((reducer * r_inv) - 1) / Q_EXT; // st R.R^-1 - q.k = 1 
+
+    logic [WIDTH - 1:0] b_;
+    logic [WIDTH - 1:0] w_;
+    logic [WIDTH - 1: 0] temp;
+    logic [2 * WIDTH:0] sum_full;
+    logic [WIDTH - 1:0] reduced;
+
+    function logic [WIDTH - 1:0] convert_in(logic [WIDTH - 1: 0] x);
+        logic [2 * WIDTH: 0] x_shifted;
+        begin
+            x_shifted = ((2 * WIDTH + 1)'(x)) << WIDTH;
+            convert_in = WIDTH'(x_shifted % (2 * WIDTH + 1)'(Q_EXT));
+        end
+    endfunction
+
+    function logic [WIDTH - 1:0] convert_out(logic [WIDTH - 1: 0] x);
+        logic [2 * WIDTH: 0] x_prod;
+        begin
+            x_prod = ((2 * WIDTH)'(x)) * ((2 * WIDTH)'(r_inv));
+            convert_out = WIDTH'(x_prod % (2 * WIDTH + 1)'(Q_EXT));
+        end
+    endfunction
+
     always_comb begin
-        p = b * w;
-        mod_mul = WIDTH'( p % (WIDTH*2)'(Q) );
+        // p = b * w;
+        // mod_mul = WIDTH'( p % (WIDTH*2)'(Q) );
+        b_ = convert_in(b);
+        w_ = convert_in(w);
+        p = b_ * w_;
+        temp = WIDTH'(((p & ((2 * WIDTH)'(reducer - 1))) * k) & ((2*WIDTH)'(reducer - 1)));
+        sum_full = {1'b0, p} + {1'b0, ((2*WIDTH)'(temp) * (2*WIDTH)'(Q_EXT))};
+        reduced  = WIDTH'(sum_full >> WIDTH);
+        mod_mul = convert_out(WIDTH'( (reduced < Q) ? reduced : reduced - Q ));
 
         sum = a + mod_mul;
         if (sum >= Q_EXT) begin
